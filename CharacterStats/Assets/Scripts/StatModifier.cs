769e58114164833eaf40a343d3970e77ded23d34
Avraam Georgiadis <me@visualabra.com>
2021-12-22 12:26:20 +0100

    Order of execution for modifiers.
    A stat modifier can now be a flat increase or a percentage increase. I used their enum int value to decide in which order they are applied. Now percentages are applied in the end.

--- CharacterStats/Assets/Scripts/StatModifier.cs
+++ CharacterStats/Assets/Scripts/StatModifier.cs
@@ -1,17 +1,18 @@
 public enum StatModType
 {
-    FlatAmmount,
-    Percentage
+	FlatAmmount,
+	Percentage
 }
 
 public class StatModifier
 {
-    public readonly StatModType ModifierType;
-    public readonly float Value;
-
-    public StatModifier(StatModType p_modifierType, float p_value)
-    {
-        ModifierType = p_modifierType;
-        Value = p_value;
-    }
+	public readonly StatModType ModifierType;
+	public readonly float Value;
+	public readonly int OrderOfExecution;
+	public StatModifier(StatModType p_modifierType, float p_value)
+	{
+		ModifierType = p_modifierType;
+		Value = p_value;
+		OrderOfExecution = (int)ModifierType;
+	}
 }
